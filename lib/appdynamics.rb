98bbcaa74d2c09035bf80bb0c39753da6fecf29b
Chris Usick <chris.usick@nfl.com>
2016-05-24 09:32:44 -0500

    initial commit

---
+++ lib/appdynamics.rb
@@ -0,0 +1,10 @@
+require 'httparty'
+require 'appdynamics/controller'
+require 'appdynamics/application'
+require 'appdynamics/node'
+require 'appdynamics/tier'
+require 'appdynamics/metric'
+
+module Appdynamics
+
+end
